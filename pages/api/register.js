52030506b8f340d8e9ffe6b9dec682c8a1ea31df
Voidxz <162740740+alejandrot31@users.noreply.github.com>
2024-05-06 22:49:45 -0400

    form

---
+++ pages/api/register.js
@@ -0,0 +1,7 @@
+//
+const sqlite3 = require('sqlite3').verbose();
+
+export default async function handler(req, res) {
+    res.status(200).json();
+  }
+  
--- pages/register.js
+++ pages/register.js
@@ -6,7 +6,15 @@ import FormInput from "@/components/formInputField";
 import { useState } from "react";
 
 export default function Register() {
+    const [usernameIsValid,setUsernameIsValid] = useState(false)
+    const [usernameError,setUsernameError] = useState("")
 
+    const [passwordIsValid,setPasswordIsValid] = useState(false)
+    const [passwordError,setPasswordError] = useState("")
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+    }
 
     return (
         <>
@@ -18,8 +26,11 @@ export default function Register() {
                     initial={{ opacity: 0, y: -50 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.5, delay: 0.05, ease: "backOut" }}
-                    className='bg-gray-800 border border-gray-800 w-1/2 p-8 rounded-lg shadow-lg'>
+                    className='bg-gray-800 border border-gray-800 w-1/2 p-8 rounded-lg shadow-lg'
+                    onSubmit={handleSubmit}
+                    >
                     
+
                     <motion.h2 
                         initial={{ opacity: 0, y: -50 }}
                         animate={{ opacity: 1, y: 0 }}
@@ -37,12 +48,17 @@ export default function Register() {
                         className='mb-4'>
                         <label htmlFor='name' className='block text-sm font-medium text-gray-300'>Username</label>
                         <div>
-                            <FormInput placeholder="Enter your username..." accentColor="border-green-500">  </FormInput>
-                            <p className="px-2 text-sm text-green-500"> Username is valid </p>
+                            <FormInput 
+                             placeholder="Enter your Username..." 
+                             accentColor={`${usernameIsValid ? 'text-green-500 border-green-500' : 'text-red-500 border-red-500'}`}
+                             onChange={(e)=> {
+                                 setUsernameIsValid(true);
+                             }}>  
+                            
+                            </FormInput>
+                            <p className={`px-2 text-sm ${usernameIsValid ? 'text-green-500' : 'text-red-500'} `}> {`${usernameIsValid ? 'Username is valid' : usernameError}`} </p>
                         </div>
                     </motion.div>
-                    
-         
                     
                     <motion.div 
                         initial={{ opacity: 0, y: -50 }}
@@ -51,8 +67,19 @@ export default function Register() {
                         className='mb-4'>
                         <label htmlFor='password' className='block text-sm font-medium text-gray-300'>Password</label>
                         <div>
-                            <FormInput password={true} placeholder="Enter your password..." accentColor="border-green-500">  </FormInput>
-                            <p className="px-2 text-sm text-green-500"> Username is valid </p>
+                            <FormInput 
+                            password={true} 
+                            placeholder="Enter your password..." 
+                            accentColor={`${passwordIsValid ? 'text-green-500 border-green-500' : 'text-red-500 border-red-500'}`}
+                            onChange={(e)=> {
+                                console.log("Changed....");
+                                setPasswordIsValid(e.target.value.length >= 6);
+                                setPasswordError(e.target.value.length >= 6 ? '' : 'Password must be at least 6 characters');
+                            }}
+                            >  
+                            
+                            </FormInput>
+                            <p className={`px-2 text-sm ${passwordIsValid ? 'text-green-500' : 'text-red-500'} `}> {`${passwordIsValid ? 'Password is valid' : passwordError}`} </p>
                         </div>
                     </motion.div>
                     
@@ -61,7 +88,7 @@ export default function Register() {
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.5, delay: .25, ease: "backOut"  }}
                         className='flex justify-end'>
-                        <Button>Register</Button>
+                        <Button type="submit">Register</Button>
                     </motion.div>
                 </motion.form>
             </div>
